6bfa5574e061823bfc001e3ed8e5d0c0a1a10d39
Christian Schwede <cschwede@redhat.com>
2023-06-26 10:31:29 +0200

    Do not create and mount empty ring ConfigMaps in SwiftRing
    The refactored rebalance script makes it possible to drop the creation
    of an initial empty ConfigMap and mounting these in the rebalance job.

--- pkg/swiftring/volumes.go
+++ pkg/swiftring/volumes.go
@@ -57,16 +57,6 @@ func getRingVolumes(instance *swiftv1beta1.SwiftRing) []corev1.Volume {
 				},
 			},
 		},
-		{
-			Name: "ring-data",
-			VolumeSource: corev1.VolumeSource{
-				ConfigMap: &corev1.ConfigMapVolumeSource{
-					LocalObjectReference: corev1.LocalObjectReference{
-						Name: swiftv1beta1.RingConfigMapName,
-					},
-				},
-			},
-		},
 	}
 }
 
@@ -90,11 +80,6 @@ func getRingVolumeMounts() []corev1.VolumeMount {
 		{
 			Name:      "ring-data-devices",
 			MountPath: "/var/lib/config-data/ring-devices",
-			ReadOnly:  true,
-		},
-		{
-			Name:      "ring-data",
-			MountPath: "/var/lib/config-data/rings",
 			ReadOnly:  true,
 		},
 	}
