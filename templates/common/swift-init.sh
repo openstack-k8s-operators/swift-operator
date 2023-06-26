8aa8d7ab85d09db6e716d4fd56082e0eba6938c6
Christian Schwede <cschwede@redhat.com>
2023-06-26 10:31:36 +0200

    Remove creating of dummy rings in swift-init.sh
    ConfigMaps are limited to 1 MiB in size, and very large deployments with
    hundreds of nodes might require a few MiBs for all ring and builder
    files. These files will be split into multiple ConfigMaps in a follow
    up, and need to be concatenated before starting service processes. This
    logic will be added in a follow up to the init script.

--- templates/common/swift-init.sh
+++ templates/common/swift-init.sh
@@ -1,17 +1,5 @@
 #!/bin/sh
-TARFILE=/var/lib/config-data/rings/swiftrings.tar.gz
-
+# NOTE: a few more functions will be added to this script, thus keeping it for now
 cp -t /etc/swift/ /var/lib/config-data/default/* /var/lib/config-data/swiftconf/*
 
-cd /etc/swift
-
-if [ ! -f $TARFILE ]; then
-    echo "$TARFILE not found - creating dummy Swift rings"
-    for f in account.builder container.builder object.builder; do
-        swift-ring-builder $f create 1 1 1
-        swift-ring-builder $f add --region 1 --zone 1 --ip 127.0.0.1 --port 0 --device dummy --weight 1
-        swift-ring-builder $f rebalance
-    done
-else
-    tar xvzf $TARFILE
-fi
+tar xvzf /var/lib/config-data/rings/swiftrings.tar.gz -C /etc/swift
