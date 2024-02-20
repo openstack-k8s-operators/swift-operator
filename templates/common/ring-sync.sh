0e80217dc2b75896fb266fff552638bad4584b94
Christian Schwede <cschwede@redhat.com>
2024-02-20 23:02:42 +0100

    Use directory based configuration files
    Separating configuration files to allow later overriding
    defaults by using additional configuration files.
    
    The SwiftStorage CR contains multiple backend services,
    and files will be copied to their intended locations based
    on their names therefore.
    
    *account-server*.conf -> account-server.conf.d/
    *container-server*.conf -> container-server.conf.d/
    *object-server*.conf -> object-server.conf.d/
    *object-expirer*.conf -> object-expirer.conf.d/
    *proxy-server*.conf -> proxy-server.conf.d/

--- templates/common/ring-sync.sh
+++ templates/common/ring-sync.sh
@@ -2,8 +2,16 @@
 TARFILE="/var/lib/config-data/rings/swiftrings.tar.gz"
 MTIME="0"
 
-# This is done only initially, should be done by Kolla probably
-cp -t /etc/swift/ /var/lib/config-data/default/* /var/lib/config-data/swiftconf/*
+# This is done only initially, will be done by Kolla eventually
+mkdir /etc/swift/account-server.conf.d /etc/swift/container-server.conf.d /etc/swift/object-server.conf.d /etc/swift/object-expirer.conf.d /etc/swift/proxy-server.conf.d
+
+cp -t /etc/swift/ /var/lib/config-data/swiftconf/swift.conf /var/lib/config-data/default/internal-client.conf /var/lib/config-data/default/rsyncd.conf
+
+cp -t /etc/swift/account-server.conf.d/ /var/lib/config-data/default/*account-server*.conf
+cp -t /etc/swift/container-server.conf.d/ /var/lib/config-data/default/*container-server*.conf
+cp -t /etc/swift/object-server.conf.d/ /var/lib/config-data/default/*object-server*.conf
+cp -t /etc/swift/object-expirer.conf.d/ /var/lib/config-data/default/*object-expirer*.conf
+cp -t /etc/swift/proxy-server.conf.d/ /var/lib/config-data/default/*proxy-server*.conf
 
 while true; do
     if [ -e $TARFILE ] ; then
