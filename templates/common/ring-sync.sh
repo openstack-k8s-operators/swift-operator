cdbdd6e26ee6b3e4ee7fa0f92ad1772602a682c3
Christian Schwede <cschwede@redhat.com>
2024-03-08 12:37:12 +0100

    Improve logging output of ring-sync.sh
    The script is used by all Swift containers, but not every config file is
    needed or available in all cases. Adding checks to avoid warnings in the
    logs that might be misleading, eg. if directories exist or files do not.
    
    Also be less verbose during normal executions, but get a list of all
    files in /etc/swift to help debugging if needed.

--- templates/common/ring-sync.sh
+++ templates/common/ring-sync.sh
@@ -2,22 +2,35 @@
 TARFILE="/var/lib/config-data/rings/swiftrings.tar.gz"
 MTIME="0"
 
-# This is done only initially, will be done by Kolla eventually
-mkdir /etc/swift/account-server.conf.d /etc/swift/container-server.conf.d /etc/swift/object-server.conf.d /etc/swift/object-expirer.conf.d /etc/swift/proxy-server.conf.d
+for f in /var/lib/config-data/swiftconf/swift.conf \
+    /var/lib/config-data/default/internal-client.conf \
+    /var/lib/config-data/default/rsyncd.conf \
+    /var/lib/config-data/default/dispersion.conf \
+    /var/lib/config-data/default/keymaster.conf; do
+    [ -e $f ] && cp -t /etc/swift/ $f
+done
 
-cp -t /etc/swift/ /var/lib/config-data/swiftconf/swift.conf /var/lib/config-data/default/internal-client.conf /var/lib/config-data/default/rsyncd.conf /var/lib/config-data/default/dispersion.conf /var/lib/config-data/default/keymaster.conf
+for s in account-server \
+    container-server \
+    object-server \
+    object-expirer \
+    proxy-server; do
+    if [ -e /var/lib/config-data/default/*${s}*.conf ]; then
+        [ ! -d  /etc/swift/${s}.conf.d ] && mkdir /etc/swift/${s}.conf.d
+        cp -t /etc/swift/${s}.conf.d/ /var/lib/config-data/default/*${s}*.conf
+    fi
+done
 
-cp -t /etc/swift/account-server.conf.d/ /var/lib/config-data/default/*account-server*.conf
-cp -t /etc/swift/container-server.conf.d/ /var/lib/config-data/default/*container-server*.conf
-cp -t /etc/swift/object-server.conf.d/ /var/lib/config-data/default/*object-server*.conf
-cp -t /etc/swift/object-expirer.conf.d/ /var/lib/config-data/default/*object-expirer*.conf
-cp -t /etc/swift/proxy-server.conf.d/ /var/lib/config-data/default/*proxy-server*.conf
-
+# This loop checks every 60 seconds if the ring files from the configmap got
+# updated and if so, extracts them /etc/swift. Swift services notice the change
+# and reload the ring files, thus no pod restart required
 while true; do
     if [ -e $TARFILE ] ; then
         _MTIME=$(stat -L --printf "%Y" $TARFILE)
         if [ $MTIME != $_MTIME ]; then
-            tar -xvzf $TARFILE -C etc/swift/
+            tar -xzf $TARFILE -C etc/swift/
+            find /etc/swift/ -type f -ls
+            echo
         fi
         MTIME=$_MTIME
     fi
